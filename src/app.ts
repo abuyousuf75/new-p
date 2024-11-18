4ee4471b64c0b490914c4188238a4d42f0f969d2
abuyousuf75 <mdabuyousufsajib@gmail.com>
2024-11-18 09:47:52 +0600

    basci project setup done

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+
+
+
+//parser
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req: Request, res: Response) => {
+  const a = 10
+  
+  
+  
+  
+  
+  
+  res.send(a);
+});
+
+console.log(process.cwd())
+export default app;
+
+//C:\Users\GEN-Z\Desktop\first-project
